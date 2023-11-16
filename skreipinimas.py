8c50457e1d431e63510ab021d32c148541fffdba
Julius0991 <64423384+Julius0991@users.noreply.github.com>
2023-11-16 09:09:19 +0100

    tarpeliu is sonu nuemimas nuo kai kuriu verciu

--- skreipinimas.py
+++ skreipinimas.py
@@ -19,7 +19,7 @@ def data_scraping():
     ligu_duomenu_lentele = []
     print('dataframe kūrimas')
     for entry in y:
-        ligu_duomenu_lentele.append([entry['_id'], entry['centras'], entry['registravimo_vieta'], entry['miestas'], entry['galutine_diagnoze'].strip(), entry['ligonis_hospitalizuotas'], entry['socialiai_apdraustas'], entry['infekcijos_tipas'], entry['is_salies'], entry['ligos_klinikine_eiga'], entry['atvyk'], entry['kreip_diag'], entry['pranesimo_menuo'], entry['mirtis'], entry['sukelejo_rusis']])
+        ligu_duomenu_lentele.append([entry['_id'], entry['centras'].strip(), entry['registravimo_vieta'].strip(), entry['miestas'].strip(), entry['galutine_diagnoze'].strip(), entry['ligonis_hospitalizuotas'], entry['socialiai_apdraustas'], entry['infekcijos_tipas'].strip(), entry['is_salies'].strip(), entry['ligos_klinikine_eiga'].strip(), entry['atvyk'], entry['kreip_diag'], entry['pranesimo_menuo'], entry['mirtis'], entry['sukelejo_rusis'].strip()])
     df = pd.DataFrame(ligu_duomenu_lentele)
     df.columns =['_id','centras', 'registravimo_vieta', 'miestas','galutine_diagnoze','ligonis_hospitalizuotas','socialiai_apdraustas','infekcijos_tipas','is_salies','ligos_klinikine_eiga','atvyk','kreip_diag','pranesimo_menuo','mirtis','sukelejo_rusis']
 
